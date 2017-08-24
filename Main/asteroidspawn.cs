26708f49736be0f9724d8c600c4ab6a9b56e4ea1
Max Shalom <max_shalom121@flatbush.org>
2017-08-24 19:50:19 -0400

    Add files via upload

---
+++ Main/asteroidspawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asteroidspawn : MonoBehaviour {
+    public float delay = 0.1f;
+    public GameObject sphere;
+	// Use this for initialization
+	void Start () {
+    InvokeRepeating("Spawn", delay, delay);
+	}
+	
+	// Update is called once per frame
+	void Spawn () {
+    Instantiate(sphere, new Vector3(Random.Range(-6, 6), 10, 3.1f),Quaternion.identity);
+	}
+}
---
+++ Main/movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+    public float speed;
+    int dir;
+
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    //Allows the object move with the keys
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            dir = 1;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            dir = -1;
+        }
+        else
+        {
+            dir = 0;
+        }
+	}
+
+   void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(dir * speed, 0, 0));
+    }
+}
